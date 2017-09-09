39c870fc4df5d3d9bbaf07b14ae6363977c30502
Eric Berdahl <berdahl@serendipity.org>
2017-09-09 15:41:55 -0700

    Updated half2 and half4 to use vecn templates.

--- API-Samples/03-init_device/03-init_device.cpp
+++ API-Samples/03-init_device/03-init_device.cpp
@@ -37,80 +37,6 @@ create and destroy a Vulkan physical device
 
 /* ============================================================================================== */
 
-class Float16Compressor
-{
-    union Bits
-    {
-        float f;
-        int32_t si;
-        uint32_t ui;
-    };
-
-    static int const shift = 13;
-    static int const shiftSign = 16;
-
-    static int32_t const infN = 0x7F800000; // flt32 infinity
-    static int32_t const maxN = 0x477FE000; // max flt16 normal as a flt32
-    static int32_t const minN = 0x38800000; // min flt16 normal as a flt32
-    static int32_t const signN = 0x80000000; // flt32 sign bit
-
-    static int32_t const infC = infN >> shift;
-    static int32_t const nanN = (infC + 1) << shift; // minimum flt16 nan as a flt32
-    static int32_t const maxC = maxN >> shift;
-    static int32_t const minC = minN >> shift;
-    static int32_t const signC = signN >> shiftSign; // flt16 sign bit
-
-    static int32_t const mulN = 0x52000000; // (1 << 23) / minN
-    static int32_t const mulC = 0x33800000; // minN / (1 << (23 - shift))
-
-    static int32_t const subC = 0x003FF; // max flt32 subnormal down shifted
-    static int32_t const norC = 0x00400; // min flt32 normal down shifted
-
-    static int32_t const maxD = infC - maxC - 1;
-    static int32_t const minD = minC - subC - 1;
-
-public:
-
-    static uint16_t compress(float value)
-    {
-        Bits v, s;
-        v.f = value;
-        uint32_t sign = v.si & signN;
-        v.si ^= sign;
-        sign >>= shiftSign; // logical shift
-        s.si = mulN;
-        s.si = s.f * v.f; // correct subnormals
-        v.si ^= (s.si ^ v.si) & -(minN > v.si);
-        v.si ^= (infN ^ v.si) & -((infN > v.si) & (v.si > maxN));
-        v.si ^= (nanN ^ v.si) & -((nanN > v.si) & (v.si > infN));
-        v.ui >>= shift; // logical shift
-        v.si ^= ((v.si - maxD) ^ v.si) & -(v.si > maxC);
-        v.si ^= ((v.si - minD) ^ v.si) & -(v.si > subC);
-        return v.ui | sign;
-    }
-
-    static float decompress(uint16_t value)
-    {
-        Bits v;
-        v.ui = value;
-        int32_t sign = v.si & signC;
-        v.si ^= sign;
-        sign <<= shiftSign;
-        v.si ^= ((v.si + minD) ^ v.si) & -(v.si > subC);
-        v.si ^= ((v.si + maxD) ^ v.si) & -(v.si > maxC);
-        Bits s;
-        s.si = mulC;
-        s.f *= v.si;
-        int32_t mask = -(norC > v.si);
-        v.si <<= shift;
-        v.si ^= (s.si ^ v.si) & mask;
-        v.si |= sign;
-        return v.f;
-    }
-};
-
-/* ============================================================================================== */
-
 template<class T>
 typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
 almost_equal(T x, T y, int ulp)
@@ -175,6 +101,8 @@ enum {
     CLK_FILTER_LINEAR = 0x0020,
     CLK_FILTER_MASK = 0x0030
 };
+
+/* ============================================================================================== */
 
 struct pipeline_layout {
     pipeline_layout() : device(VK_NULL_HANDLE), descriptors(), pipeline(VK_NULL_HANDLE) {};
@@ -258,6 +186,8 @@ struct memory_map {
     void*           data;
 };
 
+/* ============================================================================================== */
+
 template <typename T>
 struct alignas(2*sizeof(T)) vec2 {
     vec2() : x(), y() {}
@@ -329,22 +259,17 @@ typedef vec4<float> float4;
 static_assert(sizeof(float4) == 16, "bad size for float4");
 
 struct half {
+    half() : x() {}
+    half(uint16_t ui) : x(ui) {}
+    
     uint16_t    x;
 };
 static_assert(sizeof(half) == 2, "bad size for half");
 
-struct half2 {
-    uint16_t    x;
-    uint16_t    y;
-};
+typedef vec2<half> half2;
 static_assert(sizeof(half2) == 4, "bad size for half2");
 
-struct half4 {
-    uint16_t    x;
-    uint16_t    y;
-    uint16_t    z;
-    uint16_t    w;
-};
+typedef vec4<half> half4;
 static_assert(sizeof(half4) == 8, "bad size for half4");
 
 typedef unsigned short   ushort;
@@ -364,6 +289,80 @@ static_assert(sizeof(uchar2) == 2, "bad size for uchar2");
 
 typedef vec4<uchar> uchar4;
 static_assert(sizeof(uchar4) == 4, "bad size for uchar4");
+
+class Float16Compressor
+{
+    union Bits
+    {
+        float f;
+        int32_t si;
+        uint32_t ui;
+    };
+
+    static int const shift = 13;
+    static int const shiftSign = 16;
+
+    static int32_t const infN = 0x7F800000; // flt32 infinity
+    static int32_t const maxN = 0x477FE000; // max flt16 normal as a flt32
+    static int32_t const minN = 0x38800000; // min flt16 normal as a flt32
+    static int32_t const signN = 0x80000000; // flt32 sign bit
+
+    static int32_t const infC = infN >> shift;
+    static int32_t const nanN = (infC + 1) << shift; // minimum flt16 nan as a flt32
+    static int32_t const maxC = maxN >> shift;
+    static int32_t const minC = minN >> shift;
+    static int32_t const signC = signN >> shiftSign; // flt16 sign bit
+
+    static int32_t const mulN = 0x52000000; // (1 << 23) / minN
+    static int32_t const mulC = 0x33800000; // minN / (1 << (23 - shift))
+
+    static int32_t const subC = 0x003FF; // max flt32 subnormal down shifted
+    static int32_t const norC = 0x00400; // min flt32 normal down shifted
+
+    static int32_t const maxD = infC - maxC - 1;
+    static int32_t const minD = minC - subC - 1;
+
+public:
+
+    static half compress(float value)
+    {
+        Bits v, s;
+        v.f = value;
+        uint32_t sign = v.si & signN;
+        v.si ^= sign;
+        sign >>= shiftSign; // logical shift
+        s.si = mulN;
+        s.si = s.f * v.f; // correct subnormals
+        v.si ^= (s.si ^ v.si) & -(minN > v.si);
+        v.si ^= (infN ^ v.si) & -((infN > v.si) & (v.si > maxN));
+        v.si ^= (nanN ^ v.si) & -((nanN > v.si) & (v.si > infN));
+        v.ui >>= shift; // logical shift
+        v.si ^= ((v.si - maxD) ^ v.si) & -(v.si > maxC);
+        v.si ^= ((v.si - minD) ^ v.si) & -(v.si > subC);
+        return { (uint16_t) (v.ui | sign) };
+    }
+
+    static float decompress(half value)
+    {
+        Bits v;
+        v.ui = value.x;
+        int32_t sign = v.si & signC;
+        v.si ^= sign;
+        sign <<= shiftSign;
+        v.si ^= ((v.si + minD) ^ v.si) & -(v.si > subC);
+        v.si ^= ((v.si + maxD) ^ v.si) & -(v.si > maxC);
+        Bits s;
+        s.si = mulC;
+        s.f *= v.si;
+        int32_t mask = -(norC > v.si);
+        v.si <<= shift;
+        v.si ^= (s.si ^ v.si) & mask;
+        v.si |= sign;
+        return v.f;
+    }
+};
+
+/* ============================================================================================== */
 
 struct spv_map {
     struct sampler {
@@ -465,7 +464,7 @@ struct pixel_traits<float4> {
     }
 
     static float4 translate(const half& pixel) {
-        return translate((half4){ pixel.x, 0, 0, 0 });
+        return translate((half4){ pixel, 0, 0, 0 });
     }
 
     static float4 translate(const uchar4& pixel) {
