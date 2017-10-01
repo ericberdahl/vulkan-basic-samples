fdb641ca07272377f8623f801b866c310e4d1053
Eric Berdahl <berdahl@serendipity.org>
2017-09-30 20:01:01 -0700

    Wrapped legacy OpenCL #defines into enums

--- API-Samples/03-init_device/03-init_device.cpp
+++ API-Samples/03-init_device/03-init_device.cpp
@@ -1244,40 +1244,42 @@ almost_equal(T x, T y, int ulp)
 
 /* ============================================================================================== */
 
-/* cl_channel_order */
-#define CL_R                                        0x10B0
-#define CL_A                                        0x10B1
-#define CL_RG                                       0x10B2
-#define CL_RA                                       0x10B3
-#define CL_RGB                                      0x10B4
-#define CL_RGBA                                     0x10B5
-#define CL_BGRA                                     0x10B6
-#define CL_ARGB                                     0x10B7
-#define CL_INTENSITY                                0x10B8
-#define CL_LUMINANCE                                0x10B9
-#define CL_Rx                                       0x10BA
-#define CL_RGx                                      0x10BB
-#define CL_RGBx                                     0x10BC
-#define CL_DEPTH                                    0x10BD
-#define CL_DEPTH_STENCIL                            0x10BE
+enum cl_channel_order {
+    CL_R = 0x10B0,
+    CL_A = 0x10B1,
+    CL_RG = 0x10B2,
+    CL_RA = 0x10B3,
+    CL_RGB = 0x10B4,
+    CL_RGBA = 0x10B5,
+    CL_BGRA = 0x10B6,
+    CL_ARGB = 0x10B7,
+    CL_INTENSITY = 0x10B8,
+    CL_LUMINANCE = 0x10B9,
+    CL_Rx = 0x10BA,
+    CL_RGx = 0x10BB,
+    CL_RGBx = 0x10BC,
+    CL_DEPTH = 0x10BD,
+    CL_DEPTH_STENCIL = 0x10BE,
+};
 
-/* cl_channel_type */
-#define CL_SNORM_INT8                               0x10D0
-#define CL_SNORM_INT16                              0x10D1
-#define CL_UNORM_INT8                               0x10D2
-#define CL_UNORM_INT16                              0x10D3
-#define CL_UNORM_SHORT_565                          0x10D4
-#define CL_UNORM_SHORT_555                          0x10D5
-#define CL_UNORM_INT_101010                         0x10D6
-#define CL_SIGNED_INT8                              0x10D7
-#define CL_SIGNED_INT16                             0x10D8
-#define CL_SIGNED_INT32                             0x10D9
-#define CL_UNSIGNED_INT8                            0x10DA
-#define CL_UNSIGNED_INT16                           0x10DB
-#define CL_UNSIGNED_INT32                           0x10DC
-#define CL_HALF_FLOAT                               0x10DD
-#define CL_FLOAT                                    0x10DE
-#define CL_UNORM_INT24                              0x10DF
+enum cl_channel_type {
+    CL_SNORM_INT8 = 0x10D0,
+    CL_SNORM_INT16 = 0x10D1,
+    CL_UNORM_INT8 = 0x10D2,
+    CL_UNORM_INT16 = 0x10D3,
+    CL_UNORM_SHORT_565 = 0x10D4,
+    CL_UNORM_SHORT_555 = 0x10D5,
+    CL_UNORM_INT_101010 = 0x10D6,
+    CL_SIGNED_INT8 = 0x10D7,
+    CL_SIGNED_INT16 = 0x10D8,
+    CL_SIGNED_INT32 = 0x10D9,
+    CL_UNSIGNED_INT8 = 0x10DA,
+    CL_UNSIGNED_INT16 = 0x10DB,
+    CL_UNSIGNED_INT32 = 0x10DC,
+    CL_HALF_FLOAT = 0x10DD,
+    CL_FLOAT = 0x10DE,
+    CL_UNORM_INT24 = 0x10DF,
+};
 
 enum {
     CLK_ADDRESS_NONE = 0x0000,
